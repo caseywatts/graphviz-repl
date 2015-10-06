7a2f8b07e15d55127e45a95e70f7b4dd3a950b77
caseywatts <casey.s.watts@gmail.com>
2015-10-06 17:58:37 -0400

    reorder javascript, refactor etherpad code into etherpadWhisperer namespace, import svg to png code

--- public/javascripts/graphviz-repl.js
+++ public/javascripts/graphviz-repl.js
@@ -1,20 +1,8 @@
 var compiling = false;
 var needCompile = true;
 var type = 'dot';
-var settings = {
-  exportSuffix: '/export/txt',
-  importSuffix: '/import',
-  hostRoot: 'https://pad.systemli.org/p/'
-};
 
-var tutorials = [
-  {'name': 'Table of Contents'     , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/0tableOfContents.dot'},
-  {'name': '1 Simplest Diagrams'   , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/1simplestDiagrams.dot'},
-  {'name': '2 Advanced Attributes' , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/2advancedAttributes.dot'},
-  {'name': '3 With Subgraphs'      , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/3withSubgraphs.dot'},
-];
-
-
+// ROOM LOGIC
 function goToRoom(_this) {
   var roomName = $(_this).find('input').val();
   window.location = "/" + roomName;
@@ -25,8 +13,7 @@ function goToRandom() {
   window.location = "/" + randomPadName();
 }
 
-function randomPadName()
-{
+function randomPadName() {
   var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   var string_length = 10;
   var randomstring = '';
@@ -38,41 +25,60 @@ function randomPadName()
   return randomstring;
 }
 
-function txtImportPath(padName) {
-  return settings.hostRoot + padName + settings.importSuffix;
-}
 
-function txtExportPath(padName) {
-  return settings.hostRoot + padName + settings.exportSuffix;
-}
+// ETHERPAD IMPORT/EXPORT LOGIC
+var etherpadWhisperer = {
+  settings: {
+    exportSuffix: '/export/txt',
+    importSuffix: '/import',
+    hostRoot: 'https://pad.systemli.org/p/'
+  },
 
-function txtImportToPad(padName, data) {
-  var url = txtImportPath(padName);
-  $.ajax(
-    {
-      url: url,
-      type: "post",
-      processData: false,
-      async: false,
-      contentType: 'multipart/form-data; boundary=boundary',
-      accepts: {
-        text: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-      },
-      data: 'Content-Type: multipart/form-data; boundary=--boundary\r\n\r\n--boundary\r\nContent-Disposition: form-data; name="file"; filename="import.txt"\r\nContent-Type: text/plain\r\n\r\n' + data + '\r\n\r\n--boundary'
-    }
-  );
-}
+  txtImportPath: function (padName) {
+    return this.settings.hostRoot + padName + this.settings.importSuffix;
+  },
 
-function txtImportFromUrl (padName, source_url) {
-  $.get(source_url).success(function(data){
-    txtImportToPad(padName, data);
-  });
-}
+  txtExportPath: function (padName) {
+    return this.settings.hostRoot + padName + this.settings.exportSuffix;
+  },
 
-function importTutorial (number) {
-  var etherpadId = $('iframe').data('etherpad-id');
-  txtImportFromUrl(etherpadId, tutorials[number].url);
-}
+  txtImportToPad: function (padName, data) {
+    var url = this.txtImportPath(padName);
+    $.ajax(
+      {
+        url: url,
+        type: "post",
+        processData: false,
+        async: false,
+        contentType: 'multipart/form-data; boundary=boundary',
+        accepts: {
+          text: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+        },
+        data: 'Content-Type: multipart/form-data; boundary=--boundary\r\n\r\n--boundary\r\nContent-Disposition: form-data; name="file"; filename="import.txt"\r\nContent-Type: text/plain\r\n\r\n' + data + '\r\n\r\n--boundary'
+      }
+    );
+  },
+
+  txtImportFromUrl: function (padName, source_url) {
+    $.get(source_url).success(function(data){
+      this.txtImportToPad(padName, data);
+    });
+  },
+
+  tutorials: [
+    {'name': 'Table of Contents'     , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/0tableOfContents.dot'},
+    {'name': '1 Simplest Diagrams'   , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/1simplestDiagrams.dot'},
+    {'name': '2 Advanced Attributes' , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/2advancedAttributes.dot'},
+    {'name': '3 With Subgraphs'      , 'url': 'https://gist.githubusercontent.com/caseywatts/78e1577fab56d04df8cd/raw/ac81e58f707e7121d76c49b2782baad67645a4d2/3withSubgraphs.dot'},
+  ],
+
+  importTutorial: function (number) {
+    var etherpadId = $('iframe').data('etherpad-id');
+    this.txtImportFromUrl(etherpadId, this.tutorials[number].url);
+  }
+};
+
+// GRAPHVIZ RENDERER
 
 function error(text){
   var errArea = $('#msg');
@@ -132,7 +138,7 @@ function compile(dotData, cb){
 var _cachedDotData = '';
 function autoCompileDo(){
   var etherpadId = $('iframe').data('etherpad-id');
-  $.get(txtExportPath(etherpadId), function( data ) {
+  $.get(etherpadWhisperer.txtExportPath(etherpadId), function( data ) {
     var _newDotData = data;
     var _cachedDotData = loadCachedDotData(etherpadId);
     if(_cachedDotData === null){
@@ -178,6 +184,42 @@ function defaultData() {
     '   {rank=same; graphviz_repl; you}',
     '}'].join("\n");
 }
+
+
+// SVG to PNG
+
+// Takes an SVG element as target
+function svg_to_png_data(target) {
+  var ctx, mycanvas, svg_data, img, child;
+
+  // Construct an SVG image
+  var new_width = target.width.baseVal.valueInSpecifiedUnits;
+  var new_height = target.height.baseVal.valueInSpecifiedUnits;
+
+  svg_data = '<svg xmlns="http://www.w3.org/2000/svg" width="' + new_width +
+             '" height="' + new_height + '">' + target.innerHTML + '</svg>';
+  img = new Image();
+  img.src = "data:image/svg+xml;utf8," + svg_data;
+
+  // Draw the SVG image to a canvas
+  mycanvas = document.createElement('canvas');
+  mycanvas.width = new_width;
+  mycanvas.height = new_height;
+  ctx = mycanvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  // Return the canvas's data
+  return mycanvas.toDataURL("image/png");
+}
+// Takes an SVG element as target
+function svg_to_png_replace(target) {
+  var data, img;
+  data = svg_to_png_data(target);
+  img = new Image();
+  img.src = data;
+  target.parentNode.replaceChild(img, target);
+}
+
 
 $(document).ready(function(){
   autoCompileDo();
